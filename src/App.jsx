d4e5c26964ef9a29ce16d5fdd2972a39a216544b
martinrh83 <martinrh83@gmail.com>
2024-09-24 03:42:04 -0300

    Add react query

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import AppLayout from "./ui/AppLayout";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Timeslots from "./pages/Timeslots";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -34,9 +39,10 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
