5f46e939235217acb82cff0d55744321035ce686
martinrh83 <martinrh83@gmail.com>
2024-09-26 04:07:27 -0300

    Add svg icons for xl devices in booking

--- src/features/calendar/CalendarHeader.jsx
+++ src/features/calendar/CalendarHeader.jsx
@@ -1,5 +1,7 @@
 import { addMonths, subMonths, format } from "date-fns";
 import { es } from "date-fns/locale";
+import { RiArrowLeftCircleLine } from "react-icons/ri";
+import { RiArrowRightCircleLine } from "react-icons/ri";
 
 export default function CalendarHeader({ currentMonth, setCurrentMonth }) {
   const prevMonth = () => {
@@ -11,12 +13,11 @@ export default function CalendarHeader({ currentMonth, setCurrentMonth }) {
   };
   return (
     <div className="header flex justify-between items-center mb-4">
-      <button onClick={prevMonth}>Prev</button>
-      {/* Pasar la localización 'es' a format para el mes y año */}
+      <RiArrowLeftCircleLine onClick={prevMonth} className="text-3xl" />
       <h2 className="capitalize">
         {format(currentMonth, "MMMM yyyy", { locale: es })}
       </h2>
-      <button onClick={nextMonth}>Next</button>
+      <RiArrowRightCircleLine onClick={nextMonth} className="text-3xl" />
     </div>
   );
 }
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,9 +1,16 @@
-import Calendar from "../features/calendar/Calendar";
 import TimeslotsList from "../features/booking/TimeslotsList";
+import Calendar from "../features/calendar/Calendar";
+import { GiTennisBall } from "react-icons/gi";
+import { GiTennisCourt } from "react-icons/gi";
+
 export default function Booking() {
   return (
-    <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center bg-white p-7 shadow-2xl rounded-md">
-      <Calendar />
+    <div className="relative">
+      <GiTennisBall className="text-[300px] fill-lime-300 absolute -left-44 top-36 z-10 xl:block hidden" />
+      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center bg-white p-7 shadow-2xl rounded-md z-50 relative">
+        <Calendar />
+      </div>
+      <GiTennisCourt className="text-[350px] fill-stone-50 absolute -right-40 top-24 z-20 xl:block hidden" />
     </div>
   );
 }
