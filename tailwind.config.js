29477e7147c3aa3d88110badeaffcdbcb690e21e
martinrh83 <martinrh83@gmail.com>
2024-09-26 01:51:11 -0300

    Add font Black han sans for heading elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ["Black Han Sans", "sans-serif"], // Replace 'sans-serif' with a fallback font if needed
+      },
+    },
   },
   plugins: [],
 };
