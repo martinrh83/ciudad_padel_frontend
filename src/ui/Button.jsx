8e869c45512a435d7a1dbe73d051923fc8cd638d
martinrh83 <martinrh83@gmail.com>
2024-09-26 04:14:55 -0300

    Add children to props

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,10 @@
-export default function Button() {
+export default function Button({ children }) {
   return (
-    <div href="#_" className="relative inline-block text-lg group">
+    <div className="relative inline-block text-lg group mx-2">
       <span className="relative z-10 block px-5 py-3 overflow-hidden font-bold leading-tight text-stone-900 transition-colors duration-300 ease-out border-2 border-stone-900 rounded-lg group-hover:text-lime-400 uppercase">
         <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-lime-400"></span>
         <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-stone-900 group-hover:-rotate-180 ease"></span>
-        <span className="relative">Reservar</span>
+        <span className="relative">{children}</span>
       </span>
       <span
         className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-stone-900  rounded-lg group-hover:mb-0 group-hover:mr-0"
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,10 @@
+import Button from "./Button";
+
 export default function Header() {
   return (
     <header className="flex justify-between items-center bg-white border-b border-b-stone-300">
       <img src="logo.jpg" alt="Ciudad Padel Salta" className="h-24 " />
-      <button className="mr-4">Hacer reserva</button>
+      <Button>Reservar</Button>
     </header>
   );
 }
